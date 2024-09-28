ce4a897ef673a7a9e85b070f77f9772cbe94ecd4
ColtSeals <colt_pfa@hotmail.com>
2024-09-27 23:02:36 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,37 +1,35 @@
 // Lista de usuários cadastrados (simulada)
 const users = [
-  { id: "152846", name: "Luanque", email: "luanque@policiamilitar.sp.gov.br", password: "luanque" },
-  { id: "user1", name: "User One", email: "user1@example.com", password: "user123" },
-  { id: "user2", name: "User Two", email: "user2@example.com", password: "user456" },
-];
-
-// Função para validar o login
-function login(identifier, password) {
-  const user = users.find(user =>
-      (user.email === identifier || user.name === identifier || user.id === identifier) && user.password === password
-  );
-
-  if (user) {
-    // Armazena os detalhes do usuário autenticado no localStorage
-    localStorage.setItem('isAuthenticated', 'true');
-    localStorage.setItem('userName', user.name);  // Armazena o nome do usuário
-    localStorage.setItem('userId', user.id);      // Armazena o ID do usuário
-    return true;
-  } else {
-    return false;
+    { email: "luanquesantos@policiamilitar.sp.gov.br", password: "luanque" },
+    { email: "user1@example.com", password: "user123" },
+    { email: "user2@example.com", password: "user456" },
+  ];
+  
+  // Função para validar o login
+  function login(email, password) {
+    const user = users.find(user => user.email === email && user.password === password);
+    
+    if (user) {
+      // Se encontrar um usuário com o email e senha correspondentes
+      return true;
+    } else {
+      // Se não encontrar, login falhou
+      return false;
+    }
   }
-}
-
-// Função de submissão de formulário de login
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const identifier = document.getElementById('inputIdentifier').value; // Pode ser nome, email ou ID
-  const password = document.getElementById('inputChoosePassword').value;
-
-  if (login(identifier, password)) {
-    window.location.href = 'formatarBNMP.html'; // Redireciona para a página do menu
-  } else {
-    document.getElementById('loginError').classList.remove('d-none'); // Exibe erro de login
-  }
-});
+  
+  // Função de submissão de formulário de login
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const email = document.getElementById('inputEmailAddress').value;
+    const password = document.getElementById('inputChoosePassword').value;
+  
+    if (login(email, password)) {
+      localStorage.setItem('isAuthenticated', 'true'); // Armazena o token de autenticação
+      window.location.href = 'dashboard.html'; // Redireciona para a página do menu
+    } else {
+      document.getElementById('loginError').classList.remove('d-none');
+    }
+  });
+  
